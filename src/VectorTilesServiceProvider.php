aaa0e4bd20cd79032c74429caf02b959fd079981
Vitalis Feist-Wurm <vitalisfeist@Mac.dm00.office.teledata.de>
2026-04-01 15:35:58 +0200

    feat: add config file with layer, cache, and geojson settings

--- src/VectorTilesServiceProvider.php
+++ src/VectorTilesServiceProvider.php
@@ -8,11 +8,15 @@ class VectorTilesServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->mergeConfigFrom(__DIR__ . '/../config/vector-tiles.php', 'vector-tiles');
     }
 
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/vector-tiles.php' => config_path('vector-tiles.php'),
+            ], 'vector-tiles-config');
+        }
     }
 }
