c61d01626fe76b52ed0e3ace354c3821d69951c0
Vitalis Feist-Wurm <vitalisfeist@Vitaliss-MacBook-Air.fritz.box>
2026-04-01 19:33:54 +0200

    feat: add ValidateTileRequest middleware with zoom/xy bounds checking

---
+++ src/Http/Middleware/ValidateTileRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ItsJustVita\VectorTiles\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use ItsJustVita\VectorTiles\VectorTileManager;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidateTileRequest
+{
+    public function __construct(
+        protected VectorTileManager $manager,
+    ) {}
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $layerName = $request->route('layer');
+        $z = (int) $request->route('z');
+        $x = (int) $request->route('x');
+        $y = (int) $request->route('y');
+
+        $layer = $this->manager->getLayer($layerName);
+        if ($layer === null) {
+            abort(404, "Layer '{$layerName}' not found.");
+        }
+
+        if ($z < $layer->minZoom || $z > $layer->maxZoom) {
+            abort(422, "Zoom level {$z} is outside bounds [{$layer->minZoom}, {$layer->maxZoom}].");
+        }
+
+        $maxTile = (1 << $z) - 1;
+        if ($x < 0 || $x > $maxTile) {
+            abort(422, "X coordinate {$x} is outside bounds [0, {$maxTile}] for zoom {$z}.");
+        }
+        if ($y < 0 || $y > $maxTile) {
+            abort(422, "Y coordinate {$y} is outside bounds [0, {$maxTile}] for zoom {$z}.");
+        }
+
+        return $next($request);
+    }
+}
